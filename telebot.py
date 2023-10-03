086dab57f9090a031eff026042fe601fe0808b23
keith <keithneo00@gmail.com>
2023-10-04 01:16:01 +0800

    #865d69m2a[in progress] #865d69kvc[in progress] added email ml link

--- telebot.py
+++ telebot.py
@@ -18,9 +18,27 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # text = f"1) To get the chat id use /chatid\n\n\<b>To request for more features visit {link} and create and issue.</b>"
     text = (
         "1) To get the chat id use /chatid\n\n"
+        "2) To initialise email ml use /einit\n"
+        "For more information on email ml use /ehelp\n\n"
         f"<b>To request for more features visit {link} and create an issue.</b>"
     )
     await update.message.reply_html(text)
+
+async def ehelp(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    e_text = (
+        "Email ml usage:\n"
+        "1) Initialise your email for email ml model generation using the /einit [your-email]."
+    )
+    await update.message.reply_html(e_text)
+
+async def getemail(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_input = ' '.join(context.args)
+
+    if not user_input:
+        await update.message.reply_text("Please enter your email after the command")
+        return
+    
+    await update.message.reply_text(user_input)
 
 def get_token():
     dir_name = os.getcwd()
@@ -41,7 +59,9 @@ def main():
 
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CommandHandler('help', help))
+    app.add_handler(CommandHandler('ehelp', ehelp))
     app.add_handler(CommandHandler('chatid', chatId))
+    app.add_handler(CommandHandler('einit', getemail))
 
     try:
         app.run_polling()
